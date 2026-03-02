558c8e8609a9c646a5066b5aee6ac9791e02d087
Suppakorn Sirikueakunchai <s6706022510174@email.kmutnb.ac.th>
2026-03-03 01:51:23 +0700

    remove unnecessary try-catch blocks from staffController.js and render pages directly, as they are unlikely to throw errors during rendering. Also, fix the snackbar timeout in appointments-form.js to 1500ms for better user experience.

--- controllers/staffController.js
+++ controllers/staffController.js
@@ -1,35 +1,14 @@
 const renderTodaysAppointmentsPage = async (req, res) => {
-  try {
-    res.render("admin/todaysAppointment");
-  } catch (error) {
-    res.status(500).json({
-      error: "Failed to render today's appointments page",
-      details: error.message,
-    });
-  }
+  res.render("admin/todaysAppointment");
 };
 
 const renderDoctorHomePage = async (req, res) => {
-  try {
-    res.render("doctor/home");
-  } catch (error) {
-    res.status(500).json({
-      error: "Failed to render doctor's home page",
-      details: error.message,
-    });
-  }
+  res.render("doctor/home");
 };
 
 const renderDoctorAppointmentDetailsPage = async (req, res) => {
-  try {
-    const appointmentId = req.params.id;
-    res.render("doctor/appointments-detail", { appointmentId });
-  } catch (error) {
-    res.status(500).json({
-      error: "Failed to render doctor's appointment details page",
-      details: error.message,
-    });
-  }
+  const appointmentId = req.params.id;
+  res.render("doctor/appointments-detail", { appointmentId });
 };
 
 
